4b4cde26ed81c598c9797386f3e485fb67db75ac
kkotorov <45800446+kkotorov@users.noreply.github.com>
2026-02-20 16:02:41 +0100

    Initial commit

---
+++ src/main/java/com/example/bankwalletAPI/BankwalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankwalletAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankwalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankwalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bankwalletAPI/BankwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankwalletAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
